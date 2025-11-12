97ca0aaf6dd4aefe0857277dbd5a46d7db82daad
stewie <stewiemayer@hotmail.com>
2025-11-12 14:58:28 -0600

    feat: se cambia el titulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import Layout from './components/Layout';
-import React from 'react';
+import Layout from "./components/Layout";
+import React, { useEffect, useState } from "react";
+import { Channel } from "./types/channelTypes";
+import { useLazyGetChannelsQuery } from "./app/features/channelsApi";
 
 const App: React.FC = () => {
+
   return (
     <Layout>
-      <h1 className="text-3xl font-bold underline">
-        Hello, CWBT App!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Claro Video EPG</h1>
     </Layout>
   );
-}
+};
 
 export default App;
