abe4b7e90d3e7a8c99ad0c27d3de03b5c5893881
stewie <stewiemayer@hotmail.com>
2025-11-12 13:40:00 -0600

    feat: se ajusta el layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-950">
       <Header />
-      <main className="grow p-4">
+      <main className="grow p-4 flex items-center justify-center text-white">
         {children}
       </main>
       <Footer />
