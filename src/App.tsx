6d5a70049448f811dccf1f8caa17f5c5f91e6eac
stewie <stewiemayer@hotmail.com>
2025-11-13 14:44:18 -0600

    feat: se agrega EPGPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { Button } from "@headlessui/react";
 import Layout from "./components/Layout";
-import { useApp } from "./hooks/useApp";
-import { EPGModal } from "./components/epgmodal/EPGModal";
-import { EPGProvider } from "./context/EPGContext";
+import { EPGPage } from "./pages/epg/EPGPage";
 
 const App: React.FC = () => {
-
-    const {show,handleShow} = useApp()
-
   return (
     <Layout>
-      <Button onClick={handleShow} className="bg-blue-500 hover:bg-blue-700 hover:cursor-pointer text-white text-xl font-bold py-4 px-6 rounded">
-        Mostrar EPG
-      </Button>
-      <EPGProvider>
-        <EPGModal show={show} handleClose={handleShow} />
-      </EPGProvider>
+      <EPGPage />
     </Layout>
   );
 };
