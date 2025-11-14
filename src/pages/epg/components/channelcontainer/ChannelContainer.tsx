857d2596b2c83bf4dbdfb048761dde757854e101
stewie <stewiemayer@hotmail.com>
2025-11-14 17:10:35 -0600

    fix: se corrige el tamaño de los elementos

--- src/pages/epg/components/channelcontainer/ChannelContainer.tsx
+++ src/pages/epg/components/channelcontainer/ChannelContainer.tsx
@@ -8,10 +8,10 @@ export const ChannelContainer = ({ channel }: ChannelContainerProps) => {
   const { image, number, name } = channel;
   return (
     <div className="flex items-center justify-center min-h-24 max-h-24 p-2">
-      <div className="flex items-center p-4 gap-4 grow h-full bg-white/10 rounded-lg">
-        <span className="text-2xl text-white">{number}</span>
+      <div className="flex items-center p-4 gap-4 grow h-full bg-white/10 rounded-lg truncate">
+        <span className="text-xl md:text-2xl text-white">{number}</span>
         <div className="flex grow justify-center p-2">
-          <img src={image} alt={name} className="h-20" />
+          <img src={image} alt={name} className="h-14 md:h-20" />
         </div>
       </div>
     </div>
