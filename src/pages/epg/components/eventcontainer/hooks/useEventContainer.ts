8294b38de5be357dd70ba4f567be89eb5d461ec4
stewie <stewiemayer@hotmail.com>
2025-11-14 14:06:29 -0600

    feat: se crea eventContainer y su hook

---
+++ src/pages/epg/components/eventcontainer/hooks/useEventContainer.ts
@@ -0,0 +1,50 @@
+import { selectChannels, setActiveEvent } from "@/app/features/channelSlice";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import { RootState } from "@/app/store";
+import { Event } from "@/types/channelTypes";
+import dayjs from "dayjs";
+import { useCallback, useMemo } from "react";
+
+export const useEventContainer = (event:Event) => {
+  const { dateFrom, dateTo } = useAppSelector((state: RootState) =>
+    selectChannels(state)
+  );
+  const dispatch = useAppDispatch();
+
+  const width = useMemo(() => {
+    const durationArr = event.duration.split(":").map((item) => parseInt(item));
+    let minutesDuration =
+      durationArr[0] * 240 + durationArr[1] * 4 + durationArr[2] / 15;
+
+    const dateBegin = dayjs(event.date_begin);
+    const dateEnd = dayjs(event.date_end);
+
+    if (dateBegin.isBefore(dateFrom)) {
+      minutesDuration += dateBegin.diff(dateFrom, "minutes") * 4;
+    }
+
+    if (dateEnd.isAfter(dateTo)) {
+      minutesDuration -= dateEnd.diff(dateTo, "minutes") * 4;
+    }
+
+    return `${minutesDuration}px`;
+  }, [event]);
+
+  const parseDate = useCallback(
+    (date: string): string => dayjs(date).format("HH:mm"),
+    []
+  );
+
+  const schedule = useMemo(
+    () => `${parseDate(event.date_begin)}-${parseDate(event.date_end)}`,
+    [event]
+  );
+
+  const handleSetEvent = () => dispatch(setActiveEvent(event));
+
+  return {
+    width,
+    schedule,
+    handleSetEvent,
+  };
+};
