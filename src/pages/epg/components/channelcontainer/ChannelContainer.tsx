c7f0681580893f3ae05bd17186e139c50dfdbaa9
stewie <stewiemayer@hotmail.com>
2025-11-14 13:33:14 -0600

    feat: se crea el componente ChannelContainer

---
+++ src/pages/epg/components/channelcontainer/ChannelContainer.tsx
@@ -0,0 +1,19 @@
+import { Channel } from "@/types/channelTypes";
+
+interface ChannelContainerProps {
+  channel: Channel;
+}
+
+export const ChannelContainer = ({ channel }: ChannelContainerProps) => {
+  const { image, number, name } = channel;
+  return (
+    <div className="flex items-center justify-center min-h-24 max-h-24 p-2">
+      <div className="flex items-center p-4 gap-4 grow h-full bg-white/10 rounded-lg">
+        <span className="text-2xl text-white">{number}</span>
+        <div className="flex grow justify-center p-2">
+          <img src={image} alt={name} className="h-20" />
+        </div>
+      </div>
+    </div>
+  );
+};
