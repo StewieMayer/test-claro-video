59819b26dd532f67c59b9564ea24dea150fd93a7
stewie <stewiemayer@hotmail.com>
2025-11-13 22:58:45 -0600

    feat: se crea un controlador para el scroll

--- src/pages/epg/components/epgmodal/hooks/useEPGModal.ts
+++ src/pages/epg/components/epgmodal/hooks/useEPGModal.ts
@@ -1,13 +1,55 @@
 import { selectChannels } from "@/app/features/channelSlice";
 import { useAppSelector } from "@/app/hooks";
 import { RootState } from "@/app/store";
+import { useEffect, useRef } from "react";
 
 export const useEPGModal = () => {
-  const { channels } = useAppSelector((state: RootState) =>
+  const { channels, activeEvent } = useAppSelector((state: RootState) =>
     selectChannels(state)
   );
 
+  const timerRef = useRef<HTMLDivElement>(null);
+  const channelsRef = useRef<HTMLDivElement>(null);
+  const eventsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const timer = timerRef.current;
+    const channels = channelsRef.current;
+    const events = eventsRef.current;
+
+    const handleSync = () => {
+      if (events) {
+        if (timer) timer.scrollLeft = events.scrollLeft;
+        if (channels) channels.scrollTop = events.scrollTop;
+      }
+    };
+
+    const handleSyncEventsLeft = () => {
+      if (events && timer) events.scrollLeft = timer.scrollLeft;
+    };
+
+    const handleSyncEventsTop = () => {
+      if (events && channels) events.scrollTop = channels.scrollTop;
+    };
+
+    timer?.addEventListener("scroll", handleSyncEventsLeft, { passive: true });
+    channels?.addEventListener("scroll", handleSyncEventsTop, {
+      passive: true,
+    });
+    events?.addEventListener("scroll", handleSync, { passive: true });
+
+    return () => {
+      timer?.removeEventListener("scroll", handleSyncEventsLeft);
+      channels?.removeEventListener("scroll", handleSyncEventsTop);
+      events?.removeEventListener("scroll", handleSync);
+    };
+  }, [timerRef.current, eventsRef.current, channelsRef.current]);
+
   return {
     channels,
+    activeEvent,
+    timerRef,
+    channelsRef,
+    eventsRef,
   };
 };
