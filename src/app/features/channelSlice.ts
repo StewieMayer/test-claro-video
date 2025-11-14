2f15d5d2c96b27ce50580b68fad374f6b8dec674
stewie <stewiemayer@hotmail.com>
2025-11-13 22:57:53 -0600

    feat: se agregan nuevos valores al estado

--- src/app/features/channelSlice.ts
+++ src/app/features/channelSlice.ts
@@ -1,15 +1,22 @@
 import { Channel, Event } from "@/types/channelTypes";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
+import { Dayjs } from "dayjs";
 
 export type EPGContextType = {
   channels: Array<Channel>;
   activeEvent: Event | undefined;
+  scrollLeft: number;
+  dateFrom: Dayjs | undefined;
+  dateTo: Dayjs | undefined;
 };
 
 const initialState: EPGContextType = {
   channels: [],
   activeEvent: undefined,
+  scrollLeft: 0,
+  dateFrom: undefined,
+  dateTo: undefined,
 };
 
 const channelSlice = createSlice({
@@ -22,11 +29,26 @@ const channelSlice = createSlice({
     setActiveEvent: (state, action: PayloadAction<Event | undefined>) => {
       state.activeEvent = action.payload;
     },
+    setScrollLeft: (state, action: PayloadAction<number>) => {
+      state.scrollLeft = action.payload;
+    },
+    setDateFrom: (state, action: PayloadAction<Dayjs | undefined>) => {
+      state.dateFrom = action.payload;
+    },
+    setDateTo: (state, action: PayloadAction<Dayjs | undefined>) => {
+      state.dateTo = action.payload;
+    },
   },
 });
 
 export const selectChannels = (state: RootState): EPGContextType =>
   state.channels;
 
-export const { setActiveEvent, setChannels } = channelSlice.actions;
+export const {
+  setActiveEvent,
+  setChannels,
+  setScrollLeft,
+  setDateFrom,
+  setDateTo,
+} = channelSlice.actions;
 export default channelSlice.reducer;
