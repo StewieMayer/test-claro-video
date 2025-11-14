c1306312c3490009d9f38a94efe96d5d261d55e5
stewie <stewiemayer@hotmail.com>
2025-11-14 17:13:05 -0600

    fix: se corrigen tamaños

--- src/pages/epg/components/channelcontainer/ChannelContainer.tsx
+++ src/pages/epg/components/channelcontainer/ChannelContainer.tsx
@@ -9,9 +9,9 @@ export const ChannelContainer = ({ channel }: ChannelContainerProps) => {
   return (
     <div className="flex items-center justify-center min-h-24 max-h-24 p-2">
       <div className="flex items-center p-4 gap-4 grow h-full bg-white/10 rounded-lg truncate">
-        <span className="text-xl md:text-2xl text-white">{number}</span>
+        <span className="text-lg md:text-2xl text-white">{number}</span>
         <div className="flex grow justify-center p-2">
-          <img src={image} alt={name} className="h-14 md:h-20" />
+          <img src={image} alt={name} className="h-10 md:h-20" />
         </div>
       </div>
     </div>
